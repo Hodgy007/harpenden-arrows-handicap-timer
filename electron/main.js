1a05a80a9d73bab5ab4b79322205b55ff653afc3
Hodgy007 <Hodgy007@users.noreply.github.com>
2026-03-11 15:55:44 +0000

    Add Electron desktop app and GitHub Pages deployment setup

---
+++ electron/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    },
+    title: 'Harpenden Arrows Race Timer'
+  });
+
+  win.loadFile(path.join(__dirname, '../dist/harpenden-arrows-handicap-timer/index.html'));
+  win.removeMenu();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
